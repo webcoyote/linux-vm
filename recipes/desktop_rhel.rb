2c1f901ab95eeeb15083b77585f6bc440de73c27
Patrick Wyatt <pat@codeofhonor.com>
2013-02-14 20:36:13 -0800

    Turn off NetworkManager and use network service since it plays more nicely with vagrant

--- recipes/desktop_rhel.rb
+++ recipes/desktop_rhel.rb
@@ -3,51 +3,74 @@
 # Recipe:: desktop_rhel
 #
 
-include_recipe 'yum::epel'  # Enterprise Linux
-include_recipe 'yum::remi'  # For Firefox
+# Update repositories
+  include_recipe 'yum::epel'  # Enterprise Linux
+  include_recipe 'yum::remi'  # For Firefox
+  execute "yum update" do
+    command "yum update -y"
+  end
 
-execute "yum update" do
-  command "yum update -y"
-end
 
-execute "install gnome" do
-  command "yum groupinstall -y basic-desktop desktop-platform x11 fonts"
-end
+# Install Gnome desktop on X11
+  execute "install gnome" do
+    command "yum groupinstall -y basic-desktop desktop-platform x11 fonts"
+  end
+
+
+# Gnome desktop wants to use NetworkManager instead of the network service.
+# Revert back to using the network service, which makes Vagrant more happy
+  execute "stop NetworkManager" do
+    user 'root'
+    command "service NetworkManager stop || true"
+  end
+  execute "disable NetworkManager" do
+    user 'root'
+    command "sudo chkconfig NetworkManager off || true"
+  end
+  execute "start network service" do
+    user 'root'
+    command "sudo service network start || true"
+  end
+  execute "enable network service" do
+    user 'root'
+    command "sudo chkconfig network on || true"
+  end
+
 
 # Browser
-package "firefox"
+  package "firefox"
 
-# Screensaver
-search("users", "disable-screensaver:true NOT action:remove") do |u|
 
-  execute "disable screen saver for #{u['id']}" do
-    user u['id']
-    command "gconftool-2 --set -t boolean /apps/gnome-screensaver/idle_activation_enabled false"
+# Disable creensaver
+  search("users", "disable-screensaver:true NOT action:remove") do |u|
+    execute "disable screen saver for #{u['id']}" do
+      user u['id']
+      command "gconftool-2 --set -t boolean /apps/gnome-screensaver/idle_activation_enabled false"
+    end
   end
 
-end
 
 # Enable one user to login automatically to the console
-search("users", "desktop-autologin:true NOT action:remove") do |u|
+  search("users", "desktop-autologin:true NOT action:remove") do |u|
+    directory "/etc/gdm" do
+      owner 'root'
+      group 'root'
+      mode '0755'
+    end
 
-  directory "/etc/gdm" do
-    owner 'root'
-    group 'root'
-    mode '0755'
+    template "/etc/gdm/custom.conf" do
+      source "custom.conf.erb"
+      owner 'root'
+      group 'root'
+      mode '0644'
+      action :create
+      variables(:username => u['id'])
+    end
   end
 
-  template "/etc/gdm/custom.conf" do
-    source "custom.conf.erb"
-    owner 'root'
-    group 'root'
-    mode '0644'
-    action :create
-    variables(:username => u['id'])
+
+# Boot into XWindows instead of shell
+  execute "set inittab to boot into XWindows" do
+    user 'root'
+    command "sed --in-place=.bak -r -e 's/id:[0-9]+:initdefault:/id:5:initdefault:/' /etc/inittab"
   end
-
-end
-
-execute "set inittab to boot into XWindows" do
-  user 'root'
-  command "sed --in-place=.bak -r -e 's/id:[0-9]+:initdefault:/id:5:initdefault:/' /etc/inittab"
-end
