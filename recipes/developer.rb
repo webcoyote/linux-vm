73736fae017446ea9ef7fb8eca8123c7826a0b62
webcoyote <pat@codeofhonor.com>
2013-12-20 14:48:50 -0800

    added build essentials

--- recipes/developer.rb
+++ recipes/developer.rb
@@ -8,6 +8,7 @@ include_recipe 'git'      # required for this recipe to work
 include_recipe 'openssh'  # configure ssh from attributes/ssh.rb
 include_recipe 'zsh'      # a proper shell
 include_recipe 'curl'     # Internet swiss-army knife
+include_recipe 'build-essential'
 
 # Add useful packages
 package 'wget'            # like curl, only different
@@ -17,7 +18,6 @@ package 'patch'           # apply diff-patches; required for RVM & etc.
 # add your recipes here; remember they must be listed in:
 #   ../metadata.rb (for recipes from Opscode)
 #   ../Berksfile   (for recipes from other locations)
-#include_recipe 'build-essential'
 
 # Add your packages here
 #package 'ntp'
